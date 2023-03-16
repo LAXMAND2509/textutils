ddbefa49ec902f53bce346dd864cae1cc043dd94
LAXMAND2509 <114348119+LAXMAND2509@users.noreply.github.com>
2023-03-16 12:12:27 +0530

    Updates

---
+++ precache-manifest.547ff8306aedeb1185ffa62fbfd141ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4022e3abb9a65b0844a6",
+    "url": "/textutils/static/css/main.6ce92854.chunk.css"
+  },
+  {
+    "revision": "4022e3abb9a65b0844a6",
+    "url": "/textutils/static/js/main.4022e3ab.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/textutils/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/textutils/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "affa09b6d85a95074989",
+    "url": "/textutils/static/js/runtime~main.affa09b6.js"
+  },
+  {
+    "revision": "c5f69d45731a8aba627eec14cd1f8b88",
+    "url": "/textutils/index.html"
+  }
+];
